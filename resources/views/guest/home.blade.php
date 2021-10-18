c557fb3350b1379dd34eec8cbaf7853a06707723
Forklein <giuseppepisani2@gmail.com>
2021-10-18 22:32:21 +0200

    Add vue

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -3,11 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -24,6 +21,9 @@
                     @endauth
                 </div>
             @endif
+            <div id="root"></div>
         </div>
+
+        <script src="{{ asset('js/front.js') }}"></script>
     </body>
 </html>
