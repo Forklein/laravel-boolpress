2f90f16e1e85bf30a181a882b7e0fd673854586f
Forklein <giuseppepisani2@gmail.com>
2021-10-18 19:08:42 +0200

    Set PostController and other

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Title</th>
+            <th scope="col">Scritto il</th>
+          </tr>
+        </thead>
+        <tbody>
+          @forelse($posts as $post)
+          <tr>
+            <td>{{$loop->iteration}}</td>
+            <td>{{$post->title}}</td>
+            <td>{{$post->getFormattedDate('created_at')}}</td>
+          </tr>
+          @empty
+          <tr>
+            <td colspan="3">Nessun Post</td>
+          </tr>
+        </tbody>
+        @endforelse
+      </table>
+</div>
+@endsection
