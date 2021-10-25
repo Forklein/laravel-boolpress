68f83155e8d9ed204afcecb5f13165e99ca6b20b
Forklein <giuseppepisani2@gmail.com>
2021-10-25 15:37:36 +0200

    Add count for users and add this to cicle for and gen Userinfo

--- database/seeds/User_InfosTableSeeder.php
+++ database/seeds/User_InfosTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 use App\UserInfo;
 use Faker\Generator as Faker;
@@ -13,10 +14,13 @@ class User_InfosTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        for ($i = 0; $i < 16; $i++) {
+
+        $users = User::all();
+        for ($i = 0; $i < count($users); $i++) {
             $new_UserInfo = new UserInfo();
             $new_UserInfo->user_id = $i + 1;
-            $new_UserInfo->address = 'Via del test';
+            $new_UserInfo->address = 'Via inventata';
+            // $faker->address();
             $new_UserInfo->phone = $faker->phoneNumber();
             $new_UserInfo->country = 'USA';
             $new_UserInfo->save();
