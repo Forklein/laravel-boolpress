87c127abd9c81a268a363fa2c2c64b638ec64714
Forklein <giuseppepisani2@gmail.com>
2021-10-28 18:33:38 +0200

    Add info user in login page home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -14,6 +14,14 @@
                     @endif
                     {{ __("You are logged in! " . Auth::user()->name) }}
                 </div>
+                <div class="card-footer">
+                    <strong>User Info</strong>
+                    <div class="user-auth-info mt-2">
+                        <p class="m-0">Email: {{Auth::user()->email}}</p>
+                        <p class="m-0">Address: {{Auth::user()->userInfo->address}}</p>
+                        <p class="m-0">Phone: {{Auth::user()->userInfo->phone}}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
